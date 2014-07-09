861bf0ecd76723fc2627cb03a46a68456065b9d6
zhaoertao <ertaozh@cn.ibm.com>
2014-07-08 21:50:58 -0700

    to support ppc64el for ipmitool-xcat
    Former-commit-id: 60ac6a4d54f582ec77487be1a8facf56976be00e

--- ipmitool/make_deb.sh
+++ ipmitool/make_deb.sh
@@ -3,6 +3,14 @@
 tar xvfz ipmitool-1.8.11.tar.gz
 cd ipmitool-1.8.11
 cp -rL ../debian .
-dpkg-buildpackage -uc -us
+HOST_ARCH=`uname -m`
+if [ "$HOST_ARCH" = "ppc64le" ]; then
+    HOST_ARCH="ppc64el"
+elif [ "$HOST_ARCH" = "x86_64" ]; then
+    HOST_ARCH="amd64"
+else
+    HOST_ARCH="amd64"
+fi
+TARGET_ARCH=$HOST_ARCH dpkg-buildpackage -uc -us
 cd -
 rm -rf ipmitool-1.8.11
