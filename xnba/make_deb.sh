42f718a1764f6b428fbf9bee4777d711c39b0ab8
immarvin <yangsbj@cn.ibm.com>
2014-11-12 00:22:23 -0800

    enhance xnba build scripts to build xnba-undi package from existed binaries,modify the xnba to be arch independent package to support mixed cluster
    Former-commit-id: e40c545260450a69138a509aa07a2184178f0f05

--- xnba/make_deb.sh
+++ xnba/make_deb.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-tar xvfj xnba-1.0.3.tar.bz2 
-cd xnba-1.0.3
-cp -rL ../debian .
-dpkg-buildpackage -uc -us
-cd -
-rm -rf xnba-1.0.3 
+dpkg-buildpackage -uc -us -R./build/rules.fromBIN 
