df41b32ae402c1795b6df2586fa1851704cccf90
chenglch <chenglch@cn.ibm.com>
2017-06-07 17:42:08 +0800

    Upgrade conserver to 8.2.1 version
    - Deprecated the patch for sslauthority.
    - Leverage sslcacertificatefile configuration from upstream.
    - Apply init script patches from old version.
    - Changelog and package information for this new version on
      rhels and ubuntu systems.
    
    implement-feature: #18

--- conserver/make_deb.sh
+++ conserver/make_deb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-tar xvfz conserver-8.1.16.tar.gz
-cd conserver-8.1.16
+tar xvfz conserver-8.2.1.tar.gz
+cd conserver-8.2.1
 cp -rL ../debian .
 dpkg-buildpackage -uc -us
 cd -
-rm -rf conserver-8.1.16
+rm -rf conserver-8.2.1
